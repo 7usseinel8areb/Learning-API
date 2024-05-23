0a4a147f71fe7c018b02adb79eee00bcd83afce1
Hussein Elghareb <halghryb287@gmail.com>
2024-05-23 03:55:35 +0300

    CRUD operations for Department

---
+++ Day1/Day1 - Lecture/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace ITI_API_Learn.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext()
+        {
+        }
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Day1/Day1 - Lecture/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ITI_API_Learn.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Manager { get; set; }
+    }
+}
